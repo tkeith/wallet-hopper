aee2f0a2394aab13ab6b48c3865cf3b723d6f5fa
Trevor Keith <tk@tkeith.com>
2023-07-23 01:35:02 +0200

    1inch integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import axios from 'axios'
 import { createPublicClient, createWalletClient, custom, http } from 'viem'
 import { CHAINS } from './configure'
 import { walletHopperAddress } from 'abis'
+import { FusionSDK, PrivateKeyProviderConnector } from '@1inch/fusion-sdk'
+import { ethers } from 'ethers'
 
 type AssetType = 'ETH' | 'APE' | 'USDC' | 'USDT'
 
@@ -17,6 +19,13 @@ interface TransactionStatus {
     actionText: string
     actionFunction: () => void
   }
+}
+
+const tokenAddressBySymbol: Record<string, string> = {
+  USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  APE: '0x4d224452801aced8b2f0aebe155379bb5d594381',
+  USDT: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  WETH: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
 }
 
 type AsyncFunction<TArgs extends any[], TResult> = (...args: TArgs) => Promise<TResult>
@@ -37,7 +46,6 @@ function memoizeAsync<TArgs extends any[], TResult>(fn: AsyncFunction<TArgs, TRe
   }
 }
 
-
 export default function Home() {
   const [address, setAddress] = useState('')
   const [asset, setAsset] = useState<AssetType>('ETH')
@@ -53,16 +61,22 @@ export default function Home() {
 
     const chain = CHAINS[chainId]
 
+    const publicClient = () =>
+      createPublicClient({
+        chain: chain,
+        transport: http(),
+      })
+
     const [address] = await walletClient.getAddresses()
 
-    return { walletClient, chainId, chain, address }
+    return { walletClient, chainId, chain, address, publicClient }
   })
 
   useEffect(() => {
     setTransactionStatus(null)
   }, [address, asset, amount])
 
-  const checkValidity = () => {
+  const checkValidity = async () => {
     ;(async function () {
       let status: TransactionStatus | null = null
 
@@ -78,7 +92,7 @@ export default function Home() {
           if (preferredAsset.chain == (await getChainDetails()).chain.name.toLowerCase()) {
             console.log('correct chain')
             if (preferredAsset.symbol === asset) {
-              //correct chain and asset
+              console.log('correct chain, correct asset')
               status = { status: 'success' }
             } else {
               console.log('correct chain, incorrect asset')
@@ -89,8 +103,75 @@ export default function Home() {
                 suggestion: {
                   description: `To complete this transaction, swap ${asset} to ${preferredAsset.symbol}`,
                   actionText: 'Swap and send',
-                  actionFunction: () => {
-                    alert('hi')
+                  actionFunction: async () => {
+                    const caller = (await getChainDetails()).address
+                    const swapFromTokenAddress = tokenAddressBySymbol[asset]
+                    const swapToTokenAddress = tokenAddressBySymbol[preferredAsset.symbol]
+                    const swapAmount = ethers.utils.parseUnits(amount, 1).toString()
+                    const url = `https://api.1inch.io/v5.2/1/swap?src=${swapFromTokenAddress}&dst=${swapToTokenAddress}&amount=${swapAmount}&from=${caller}&slippage=1&disableEstimate=false&includeTokensInfo=true&includeProtocols=true&compatibility=true&allowPartialFill=false`
+                    let response
+                    try {
+                      response = await axios.get(url)
+                    } catch (error) {
+                      toast.error('failed to generate swap')
+                      return
+                    }
+                    console.log(response.data)
+                    const swapData = response.data.tx
+                    const walletClient = (await getChainDetails()).walletClient
+                    const sendForSwap = {
+                      // chain: await walletClient.getChainId(),
+                      // account: (await getChainDetails()).address,
+                      from: swapData.from,
+                      to: swapData.to,
+                      data: swapData.data,
+                      value: swapData.value,
+                    }
+                    console.log(sendForSwap)
+                    // let txnHash = await walletClient.sendTransaction(sendForSwap)
+                    // send txn using ethers
+                    const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+                    const signer = provider.getSigner()
+                    let txn
+                    try {
+                      txn = await signer.sendTransaction(sendForSwap)
+                    } catch (error) {
+                      toast.error('failed to generate swap')
+                      return
+                    }
+                    let txnHash = txn.hash
+
+                    toast.info(`Generated swap transaction, waiting for success...`)
+                    while (true) {
+                      try {
+                        // @ts-ignore
+                        await (await getChainDetails()).publicClient().waitForTransactionReceipt({ hash: txnHash })
+                        break
+                      } catch (error) {
+                        // async sleep 3 seconds
+                        await new Promise((resolve) => setTimeout(resolve, 3000))
+                      }
+                    }
+
+                    // now send the received ERC-20 token
+                    const tokenContract = new ethers.Contract(
+                      swapToTokenAddress,
+                      ['function transfer(address to, uint256 amount)'],
+                      new ethers.providers.Web3Provider((window as any).ethereum)
+                    )
+                    txnHash = await tokenContract.transfer(address, swapAmount)
+
+                    toast.info(`Generated send transaction, waiting for success...`)
+                    while (true) {
+                      try {
+                        // @ts-ignore
+                        await (await getChainDetails()).publicClient().waitForTransactionReceipt({ hash: txnHash })
+                        break
+                      } catch (error) {
+                        // async sleep 3 seconds
+                        await new Promise((resolve) => setTimeout(resolve, 3000))
+                      }
+                    }
                   },
                 },
               }
