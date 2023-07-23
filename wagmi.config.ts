64e48d2bda43beddc7a08014efa7e1d6fa3821a8
Trevor Keith <tk@tkeith.com>
2023-07-23 03:22:12 +0200

    deployments

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -15,7 +15,10 @@ export default defineConfig({
       project: './contracts',
       deployments: {
         WalletHopper: {
-          137: '0x74156E7CCe407169797053E1A1A17B0C1F920a90',
+          137: '0x74156E7CCe407169797053E1A1A17B0C1F920a90', // polygon
+          59144: '0x898B1a78A78A17dE24a63372149C391Ac90fbddc', // linea
+          1101: '0x8193A58Cf26bbD0A865B83EdB004e2C138260AFc', // zkevm
+          100: '0x898B1a78A78A17dE24a63372149C391Ac90fbddc', // gnosis
         },
       },
     }),
